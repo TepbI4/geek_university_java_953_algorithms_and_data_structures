f0acff6e397633510ffa26fa899c343a0c82ab9c
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-12-08 01:02:29 +0400

    hw3

--- hw3/src/main/java/ru/gb/alekseiterentev/FindMissingIntApp.java
+++ hw3/src/main/java/ru/gb/alekseiterentev/FindMissingIntApp.java
@@ -4,16 +4,20 @@ public class FindMissingIntApp {
 
     public static void main(String[] args) {
         int[] input = {1,2,3,4,5,6,7,8,9,10,12,13,14,15,16};
-        int missingInt = getMissingInt(input, 0, input.length-1);
-        System.out.println(missingInt);
-    }
 
-    public static int getMissingInt(int[] arr, int left, int right) {
-        if (right == left + 1) return arr[right] - 1;
-        int base = left + (right - left) / 2;
-        if (arr[base] == arr[left] + (arr[right] - arr[left]) / 2 - (right - left) % 2)
-            return getMissingInt(arr, base, right);
-        else
-            return getMissingInt(arr, left, base);
+        if (input.length > 0) {
+            int left = 0;
+            int right = input.length-1;
+            while (right != left + 1) {
+                int base = left + (right - left) / 2;
+                if (input[base] == input[left] + (input[right] - input[left]) / 2 - (right - left) % 2)
+                    left = base;
+                else
+                    right = base;
+            }
+            System.out.println(input[right] - 1);
+        } else {
+            System.out.println(1);
+        }
     }
 }
